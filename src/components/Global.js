d3fde7c828f0491540a4130d816038ae262e00dd
strojac <strosahljack@gmail.com>
2024-11-12 15:51:35 -0500

    Revert "Add workshop card"
    This reverts commit caa6a9efc5000af2cd20b0f47cdecdc71a4c680a.

--- src/components/Global.js
+++ src/components/Global.js
@@ -34,13 +34,15 @@ class Global extends React.Component
     render()
     {
         return(
-            <div style={{width:"100%", marginBottom: "1%"}}>
+            <div style={{width:"100%",display:"flex"}}>
                 <Head/>
-                <Navbar variant="dark" bg="dark" style={{fontSize:"2vh",width:"100%",position:"relative"}}>
+                <Navbar variant= "dark" bg="dark" style={{fontSize:"2vh",width:"100%",position:"relative",padding:"0vh 1vw 0vh 1vw"}}>
                     <Navbar.Brand as={Link} style={{fontSize:"3vh",lineHeight:"5vh",fontFamily:"var(--font-family-sans-serif)",fontWeight:"500",width:"0"}} to="" name="">{"{js}"}</Navbar.Brand>
                     <Container fluid style={{alignContent:"center",justifyContent:"center"}}>
                         <Nav variant="pills" bg="light" activeKey={this.getActiveKey()} style={{alignContent:"center",fontWeight:"700"}}>
                             <h1 style={{backgroundImage:"linear-gradient(to right, purple,orange)",backgroundClip:"text",WebkitBackgroundClip:"text",color:"transparent"}}>jstro.io</h1>
+                            {/*<HeaderLink to="posts" name="Posts"/>*/}
+                            {/*<HeaderLink to="map" name="Map"/>*/}
                         </Nav>
                     </Container>
                 </Navbar>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,61 @@
 import React from'react'
-import Card from 'react-bootstrap/Card';
 import Global from '../components/Global.js'
-import YouTube from "../components/YouTube"
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import {Link} from 'gatsby'
 
 class IndexPage extends React.Component
 {
-
-  randItem(arr)
-  {
-      return arr[Math.floor(Math.random()*arr.length)];
-  }
-  render =()=>
-  {
-      return(
-          <div style={{height:"100vh", display:"flex",flexDirection:"column"}}>
-              <Global/>
-              <div className="hc">
-                <Card bg="warning">
-                  <Card.Header className="hc">Generative Art Workshop</Card.Header>
-                  <Card.Body>
-                    <Card.Text className="hc">
-                      I hosted a Generative Art Workshop with Communication Madison <br/> in Fall 2020, you can see my tutorials below.
-                    </Card.Text>
-                    <div className="vc hc">
-                      <YouTube id="xuYaILT_BTI">
-                        First
-                      </YouTube>
-                      <YouTube id="M2RhNl0gLXo">
-                      Second
-                      </YouTube>
-                      <YouTube id="bBnG1Vhccpo">
-                        Third
-                      </YouTube>
-                    </div>
-                  </Card.Body>
-                </Card>
-
-              </div>
-          </div>
-      );
-  }
+    data = null;
+    constructor({data})
+    {
+        super();
+        this.data=data;
+    }
+    randItem(arr)
+    {
+        return arr[Math.floor(Math.random()*arr.length)];
+    }
+    render =()=>
+    {
+        return(
+            <div style={{height:"100vh",display:"flex",flexDirection:"column"}}>
+                <Global/>
+                <div style={{position:"relative",overflow:"visible",height:"0px",width:"0px"}}>
+                    <iframe title="Swirling Colors Animation" src="https://editor.p5js.org/jackstrosahl/embed/G_-1c39p2"
+                        style={{position:"fixed",width:"100%",height:"100%", zIndex:"-100"}}/>
+                </div>
+                <div className="vc hc full">
+                  <Card bg="primary" text="white" className="text-center">
+                    <Card.Body>
+                      <Card.Body>
+                        Check me out on YouTube!
+                      </Card.Body>
+                      <Button variant="danger" href="https://www.youtube.com/channel/UC_30B5_Jc9i5UIrG596Lqdg?sub_confirmation=1">Subscribe</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+                {/*
+                <BackgroundImage fluid={this.data.hanging.childImageSharp.fluid}className="vc hc" style={{flex:"1",overflow:"hidden"}}>
+                <Button variant="danger" style={{display:"flex",fontSize:"3vh",maxHeight:"5vh",lineHeight:"1"}} onClick={()=>(this.forceUpdate())}>
+                        jstro.io
+                </Button>
+                </BackgroundImage>*/}
+            </div>
+        );
+    }
 }
 export default IndexPage;
+
+export const query = graphql`
+  query
+  {
+    hanging: file(relativePath: {eq: "hanging.jpg"}) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+            ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
